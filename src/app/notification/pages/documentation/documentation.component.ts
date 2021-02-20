4a263fd41ce087cc805a52180b5d6e9241351f88
Juan Falcón <JUANCANEN_10_@HOTMAIL.COM>
2021-02-20 10:46:18 -0500

    Added logic checkbox

--- src/app/notification/pages/documentation/documentation.component.ts
+++ src/app/notification/pages/documentation/documentation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
+
 @Component({
   selector: 'app-documentation',
   templateUrl: './documentation.component.html',
@@ -8,10 +9,23 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class DocumentationComponent implements OnInit {
   myForm: FormGroup;
+  dataFake = [
+    {
+      value: 'level_1',
+      checked: true
+    },
+    {
+      value: 'level_2',
+      checked: false
+    },
+    {
+      value: 'level_3',
+      checked: false
+    }
+  ]
 
+  constructor() {
 
-  constructor() { 
-    
   }
 
   ngOnInit() {
@@ -29,5 +43,47 @@ export class DocumentationComponent implements OnInit {
     console.log('Email', form.value.email);
     console.log('Message', form.value.message);
   }
+
+  sendDataLevel() {
+    let me = this;
+    const dataCheckBox = me.dataValidate(me.dataFake);
+    console.log(dataCheckBox)
+    if (dataCheckBox.length == 0) {
+      console.log('checkbox invalid');
+      return;
+    }
+    console.log('data checbox', { data: dataCheckBox[0] });
+  }
+
+  changeCheckbox(event, checked) {
+    let me = this;
+    me.dataFake = me.clearValueCheckBoxTrue(me.dataFake);
+    me.dataFake = me.dataFake.map((e) => {
+      if (e.value == event.target.id) {
+        return { value: e.value, checked }
+      } else {
+        return { value: e.value, checked: false }
+      }
+    });
+    console.log(event.target.id, me.dataFake)
+  }
+
+  clearValueCheckBoxTrue(data: any[]) {
+    let me = this;
+    const newArrary = [];
+    data.map((e) => {
+      return newArrary.push({ value: e.value, checked: false });
+    });
+    return newArrary;
+  }
+
+  dataValidate(data: any[]) {
+    const newArrary = [];
+    data.filter((e) => e.checked).forEach((e) => {
+      newArrary.push(e.value)
+    });
+    return newArrary
+  }
+
 
 }
